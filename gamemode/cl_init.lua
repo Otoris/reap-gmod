22c5e686223ce45223b5d9db79129904ba1ca4ce
sethschn301 <sethschn301@f9c5997b-c900-781d-ac18-e5a75adddefc>
2010-11-16 04:38:10 +0000

    Added PlayerCreateMenu allowing you to select your model. Still need to send it serverside to set it though

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -7,30 +7,47 @@ end
 function PlayerCreateMenu()
 	local DermaPanel = vgui.Create( "DFrame" )
 	DermaPanel:SetPos( 50,50 )
-	DermaPanel:SetSize( 500, 700 )
-	DermaPanel:SetTitle( "Testing Derma Stuff" )
+	DermaPanel:SetSize( 400, 600 )
+	DermaPanel:SetTitle( "Reap Model Selector" )
 	DermaPanel:SetVisible( true )
 	DermaPanel:SetDraggable( true )
 	DermaPanel:ShowCloseButton( true )
 	DermaPanel:MakePopup()
 
+	local icon = vgui.Create( "DModelPanel", DermaPanel )
+	icon:SetModel( "models/player/Group01/male_01.mdl" )
+	icon:SetPos(-50,55)
+	icon:SetSize( 500, 500 )
+	icon:SetCamPos( Vector( 100, 0, 110 ) )
+	icon:SetLookAt( Vector( 0, 0, 0 ) )
+	
 	local DermaListView = vgui.Create("DListView")
 	DermaListView:SetParent(DermaPanel)
-	DermaListView:SetPos(25, 50)
-	DermaListView:SetSize(300, 400)
+	DermaListView:SetPos(50, 350)
+	DermaListView:SetSize(300, 200)
 	DermaListView:SetMultiSelect(false)
 	DermaListView:AddColumn("Model") -- Add column
+	
 
-	for k,v in pairs(player.GetAll()) do
-		DermaListView:AddLine(v:Nick()) -- Add lines
+	for k,v in pairs(MaleModels) do
+		DermaListView:AddLine("models/player/Group01/male_0"..MaleModels[k]) -- Add lines
 	end
 	
-	 
-	local icon = vgui.Create( "DModelPanel", DermaPanel )
-	icon:SetModel( LocalPlayer():GetModel() )
-	 
-	icon:SetSize( 100, 100 )
-	icon:SetCamPos( Vector( 50, 50, 120 ) )
-	icon:SetLookAt( Vector( 0, 0, 0 ) )
+	for k,v in pairs(MaleModels) do
+		DermaListView:AddLine("models/player/Group03/male_0"..MaleModels[k]) -- Add lines
+	end
+	
+	for k,v in pairs(FemaleModels) do
+		DermaListView:AddLine("models/player/Group01/female_0"..FemaleModels[k]) -- Add lines
+	end
+	
+	for k,v in pairs(FemaleModels) do
+		DermaListView:AddLine("models/player/Group03/female_0"..FemaleModels[k]) -- Add lines
+	end
+	
+	DermaListView.OnClickLine = function(parent, line, isselected) //We override the function with our own
+		icon:SetModel(line:GetValue(1)..".mdl")
+	end
+
 
 end
--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -7,3 +7,9 @@ StartingHP = 50
 StartingArmor = 0
 StartingMoney = 0
 StartingInventory = {}
+
+
+--models\player\Group01\male_NUM
+--models\player\Group03\male_NUM
+MaleModels = {1,2,3,4,5,6,7,8,9}
+FemaleModels = { 1, 2, 3, 4, 6, 7 }
